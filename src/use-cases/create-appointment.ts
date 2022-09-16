944baccf4cb9ca97436c359c6750a52fc941b2a6
SergioVago <sergio.vago@hotmail.com>
2022-09-16 20:35:54 -0300

    Fim do vídeo

---
+++ src/use-cases/create-appointment.ts
@@ -0,0 +1,41 @@
+import { Appointment } from '../entities/appointment'
+import { AppointmentsRepository } from '../repositories/appointments-repository'
+
+interface CreateAppointmentRequest {
+  customer: string
+  startsAt: Date
+  endsAt: Date
+}
+
+type CreateAppointmentResponse = Appointment
+
+export class CreateAppointment {
+  constructor (
+    private readonly appointmentsRepository: AppointmentsRepository
+  ) {}
+
+  async execute ({
+    customer,
+    startsAt,
+    endsAt
+  }: CreateAppointmentRequest): Promise<CreateAppointmentResponse> {
+    const overlappingAppointment = await this.appointmentsRepository.findOverlappingAppointment(
+      startsAt,
+      endsAt
+    )
+
+    if (overlappingAppointment) {
+      throw new Error('Another appointment overlaps this appointment date')
+    }
+
+    const appointment = new Appointment({
+      customer,
+      startsAt,
+      endsAt
+    })
+
+    await this.appointmentsRepository.create(appointment)
+
+    return appointment
+  }
+}
